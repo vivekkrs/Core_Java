9f97f5a00b999e27e8f45fcdf428b4280af9b75b
Vivek Kumar <vivekkr5598@gmail.com>
2026-01-02 11:42:39 +0530

    first commit

--- src/Loop/Whileloop.java
+++ src/Loop/Whileloop.java
@@ -2,10 +2,16 @@ package Loop;
 
 public class Whileloop {
     public static void main(String[] args) {
+//        int i=1;
+//        while (i<=10){
+//            System.out.print(i+" ");
+//            i++;
+//        }
+        // do while loop
         int i=1;
-        while (i<=10){
+        do{
             System.out.print(i+" ");
             i++;
-        }
+        }while (i<=10);
     }
 }
