147d2ee81ff9fa8f3eb16642ed966a2a67a35ba1
Vivek Kumar <vivekkr5598@gmail.com>
2026-01-02 11:42:39 +0530

    first commit

---
+++ src/Loop/Continue.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class Continue {
+    public static void main(String[] args) {
+        for(int i=1; i<=20; i++){
+            System.out.println(i);
+            if(i==13) continue;
+            System.out.println("Good Morning.");
+        }
+    }
+}
